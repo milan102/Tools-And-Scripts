a16edbebbdc5d17a729b33da817e7c19c0b6dd3d
Milan <milan102@users.noreply.github.com>
2020-05-02 22:17:59 -0700

    Update get-youtube-urls-from-channel-search.js

--- get-youtube-urls-from-channel-search.js
+++ get-youtube-urls-from-channel-search.js
@@ -6,4 +6,4 @@ for(let i = 0; i < searchResults.length; i++) {
   links.push(searchResults[i].href);
 }
 
-links.toString().split(",").join("\n");
+const formattedLinks = links.toString().split(",").join("\n");
